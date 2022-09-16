bc4a1d636e4d5bd0d6520c64218625f8472c93a5
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2022-09-16 07:39:11 -0700

    Go/Golang

---
+++ go/main.go
@@ -0,0 +1,12 @@
+package main // dosyayı hangi klasöre yerleştireceğimizi belirliyoruz.
+import "fmt" // fmt paketini kullanmak için projemize import ediyoruz.
+
+func main() {
+	// main bloğu go kodlarının çalışacağı bloktur.
+	// go satır base'li bir dildir.
+	// fmt=format
+	// go büyük küçük harf duyarlı bir dildir.
+	// kullanmadığın bir şeyi import edemezsin.
+
+	fmt.Println("Cahit")
+}
