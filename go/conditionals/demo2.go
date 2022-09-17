23b16110658788ab700b2f7a0a127e490855a1ec
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2022-09-17 01:32:32 -0700

    Go/Golang

--- go/conditionals/demo2.go
+++ go/conditionals/demo2.go
@@ -2,15 +2,22 @@ package conditionals
 
 import "fmt"
 
-func Demo1() {
-
+func Demo2() {
 	hesap := 1000
 	cekilmekIstenen := 1900
 
 	if cekilmekIstenen > hesap {
 		fmt.Print("Hesaptaki Para Yetersiz")
+	} else {
+		fmt.Print("Paranız Hazırlanıyor")
 	}
-	//%f float %v value
-	//fmt.Println("Bitti. Hesaptaki Para: " + fmt.Sprint("%v", hesap))
-	fmt.Printf("Hesaptaki Para: %v", hesap)
+
+	/*
+		if sart{
+			if sart{
+
+			}
+
+		}
+	*/
 }
--- go/main.go
+++ go/main.go
@@ -8,5 +8,5 @@ import (
 
 func main() {
 	variables.Demo1()
-	conditionals.Demo1()
+	conditionals.Demo2()
 }
