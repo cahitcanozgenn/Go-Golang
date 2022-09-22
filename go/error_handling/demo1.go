c1a1ce8415a62cee16ec638a0313a676fc7b2501
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2022-09-22 02:04:03 -0700

    Go/Golang

--- go/error_handling/demo1.go
+++ go/error_handling/demo1.go
@@ -5,12 +5,19 @@ import (
 	"os"
 )
 
+// tyoe assertion
 func Demo1() {
-	f, error := os.Open("demo1.txt")
+	f, error := os.Open("demoe1.txt")
 
 	//dosya bulunursa f olur. error nil olur.
 	// hata yoksa nil
 	if error != nil {
+		if pErr, ok := error.(*os.PathError); ok {
+			fmt.Println("Dosya Bulunamadı", pErr)
+			return
+		} else {
+			fmt.Println("Dosya Bulunamadı")
+		}
 		fmt.Println("Dosya Bulunamadı")
 	} else {
 		fmt.Println(f.Name())
